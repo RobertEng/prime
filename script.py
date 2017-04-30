8d870f8fa154b0dbdb77156a8b6fb7156aae1697
Robert Eng <robert.km.eng@gmail.com>
2017-04-30 04:31:53 -0700

    first commit

---
+++ script.py
@@ -0,0 +1,12 @@
+import pp
+ 
+ppservers = ("*",)  # autodiscovery mode on!
+ 
+# create the job server
+job_server = pp.Server(ppservers=ppservers)
+ 
+print "Starting pp! Local machine has {} workers (cores) available.".format(job_server.get_ncpus())
+
+
+for computer, cpu_count in job_server.get_active_nodes().iteritems():
+    print "Found {} with CPU count {}!".format(computer, cpu_count)
