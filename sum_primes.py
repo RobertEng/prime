75e7d29eb5a5a08dba217f414efe48c40f811b68
Robert Eng <robert.km.eng@gmail.com>
2017-04-30 05:13:17 -0700

    fix tuple bug

--- sum_primes.py
+++ sum_primes.py
@@ -46,10 +46,10 @@ start_time = time.time()
 # inputs = (100000, 100100, 100200, 100300, 100400, 100500, 100600, 100700)
 inputs = "171 40 133 169 75 8 73 187 195 48 89 151 54 63 90 33 68 81 118 24 159 7 146 15 4 156 31 134 35 109 62 158 95 143 12 74 77 17 126 139 128 101 39 100 14 172 167 49 175 141 145 19 130 170 148 179 165 157 83 161 153 121 105 46 85 164"
 inputs = [int(i) + 100000 for i in inputs.split(' ')]
-
+inputs = tuple(inputs)
 jobs = []
  
-jobs.append(job_server.submit(sum_primes, (input,), (isprime,), ("math",)))
+jobs.append(job_server.submit(sum_primes, (inputs,), (isprime,), ("math",)))
  
 # gather results
 for job in jobs:
@@ -61,7 +61,7 @@ if result:
     print result
 else:
     print "Sorry, couldn't find anything."
-    
+
 
 # jobs = [(input, job_server.submit(sum_primes, (input,), (isprime,), ("math",))) for input in inputs]
 # for input, job in jobs:
